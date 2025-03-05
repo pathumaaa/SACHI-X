8ad967f914efeb47fd351c274266c2c4bc72721c
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-03-05 14:06:50 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+# Automatically populated with IP and Port during installation
+PANEL_IP = 'auto-detected-ip'  # Automatically detected IP address (VPS IP)
+PANEL_PORT = 'user-provided-port'  # User-specified panel port
