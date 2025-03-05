4cf24afbb80b6be7c845069b17c780b4f19f9887
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-03-05 12:09:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,4 @@
 import os
-import json
-
-def set_x_ui_url(url):
-    # Save the URL in an environment variable or a config file
-    config = {'X_UI_URL': url}
-    with open('config.json', 'w') as f:
-        json.dump(config, f)
 
 def get_x_ui_url():
-    # Load the URL from the configuration file
-    if os.path.exists('config.json'):
-        with open('config.json', 'r') as f:
-            config = json.load(f)
-            return config.get('X_UI_URL', 'http://127.0.0.1')
-    else:
-        return 'http://127.0.0.1'
+    return "https://158.178.241.130"  # Use HTTPS instead of HTTP
