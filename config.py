ae5b870061e161b34987af625ec7326470555492
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-03-06 18:01:41 +0530

    Delete config.py

--- config.py
+++
@@ -1,3 +0,0 @@
-# Automatically populated with IP and Port during installation
-PANEL_IP = 'auto-detected-ip'  # Automatically detected IP address (VPS IP)
-PANEL_PORT = 'user-provided-port'  # User-specified panel port
