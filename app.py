7d0c74ade9c2a4d93d9c86ace7c3172a27538a86
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-03-10 14:17:41 +0530

    web

--- app.py
+++ app.py
@@ -1,3 +1,13 @@
+# @fileOverview Check usage stats of X-SL
+# @author MasterHide
+# @Copyright © 2025 x404 MASTER™
+# @license MIT
+#
+# You may not reproduce or distribute this work, in whole or in part, 
+# without the express written consent of the copyright owner.
+#
+# For more information, visit: https://t.me/Dark_Evi
+
 from flask import Flask, request, render_template, jsonify
 import sqlite3
 import json
