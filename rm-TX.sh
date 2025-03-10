6cd68d13675cf15c27b7daab596f8512a22b625c
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-03-10 14:17:12 +0530

    out

--- rm-TX.sh
+++ rm-TX.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# @fileOverview Check usage stats of X-SL
+# @author MasterHide
+# @Copyright © 2025 x404 MASTER™
+# @license MIT
+#
+# You may not reproduce or distribute this work, in whole or in part, 
+# without the express written consent of the copyright owner.
+#
+# For more information, visit: https://t.me/Dark_Evi
+
+
 # Ask user for confirmation
 echo "This script will uninstall Traffic-X and all associated files. Are you sure you want to proceed? (y/n)"
 read CONFIRM
