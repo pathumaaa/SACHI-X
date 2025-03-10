eb1f6989c90a0915653b1355c4c016af8fe6076a
x404 MASTER <71309968+MasterHide@users.noreply.github.com>
2025-03-10 14:16:51 +0530

    in

--- Traffic-X.sh
+++ Traffic-X.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# @fileOverview Check usage stats of X-SL
+# @author MasterHide
+# @Copyright © 2025 x404 MASTER™
+# @license MIT
+#
+# You may not reproduce or distribute this work, in whole or in part, 
+# without the express written consent of the copyright owner.
+#
+# For more information, visit: https://t.me/Dark_Evi
+
+
 # Function to display the menu
 show_menu() {
     echo "Welcome to Traffic-X Installer/Uninstaller"
